3bfc8551215d370438be2801e72857215ba366b4
Matt <Matt@DESKTOP-0SIUBJ5>
2017-07-03 00:29:19 -0600

    fuck riunign out of comments whehre are new colors

--- Ranxocdmdo/Source.cpp
+++ Ranxocdmdo/Source.cpp
@@ -9,13 +9,14 @@ int main()
 	int Range;
 	int ToFind = 0;
 	srand(GetTickCount());
-	system("color 0a");
+	system("color 4F");
 	cout << "choose a Range niger \n";
 	cin >> Range;
 	system("cls");
 	cout << "if you like stalin choose a delay! \n";
 	cin >> delay;
 	system("cls");
+	system("color 10");
 	while (ToFind != Range)
 	{
 		ToFind = rand() % (Range+1);
