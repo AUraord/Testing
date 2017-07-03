5e43eeb9d7c58de312c43ba3ff1a000b5d248857
Matt <Matt@DESKTOP-0SIUBJ5>
2017-07-03 00:17:34 -0600

    WOW LOOK I MEESSED UP?

--- Ranxocdmdo/Source.cpp
+++ Ranxocdmdo/Source.cpp
@@ -5,13 +5,20 @@ using namespace std;
 
 int main()
 {
+	int d;
+	int x;
 	int z = 0;
 	srand(GetTickCount());
 	system("color 0a");
+	cout << "choose a number to find niger \n";
+	cin >> x;
+	cout << "if you like stalin choose a delay! \n";
+	cin >> d;
 	while (z != 1)
 	{
-		z = rand() % (100 + 1);
-		cout << z << "\t";
+		z = rand() % (100 + x);
+		Sleep(d);
+		cout << z << "	";
 	}
 	system("pause");
 }
