eb71cbfe41b1e69cb9ecfc49d2e0c5be13046e55
Matt <Matt@DESKTOP-0SIUBJ5>
2017-07-04 00:32:58 -0600

    Presentlebalabbel.gaypeg

--- Ranxocdmdo/Source.cpp
+++ Ranxocdmdo/Source.cpp
@@ -6,19 +6,19 @@ using namespace std;
 
 int main()
 {
-	
-	
-	int ToFind = 0;
 	HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
 
 	SetConsoleTextAttribute(hCon, FOREGROUND_RED | FOREGROUND_INTENSITY);
 
+	int ToFind = 0;
 	cout << "choose a number for this program to find! \n";
 	cin >> ToFind;
 	system("cls");
 
 	SetConsoleTextAttribute(hCon, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+
 	int Range;
+
 	cout << "choose a Range \n";
 	cin >> Range;
 	system("cls");
@@ -27,7 +27,9 @@ int main()
 		return 1;
 
 	SetConsoleTextAttribute(hCon, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+
 	int Delay;
+
 	cout << "if you like stalin choose a delay! \n";
 	cin >> Delay;
 	system("cls");
@@ -38,13 +40,18 @@ int main()
 	int MinNumber = 0;
 	int TimesToFind = 0;
 	int MaxNumber = 0;
+
 	SetConsoleTextAttribute(hCon, FOREGROUND_GREEN  | BACKGROUND_RED | FOREGROUND_INTENSITY);
+
 	while (ToFind != Range)
 	{
 		ToFind = rand() % (Range + 1);
-		Sleep(Delay);
-		cout << ToFind  << "	";
+
 		TimesToFind++;
+		Sleep(Delay);
+
+		cout << ToFind  << "	";
+
 		if (ToFind > MaxNumber)
 			MaxNumber = ToFind;
 		if (ToFind < MinNumber)
@@ -55,9 +62,14 @@ int main()
 	SetConsoleTextAttribute(hCon, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
 
 	cout << endl << "it took " << TimesToFind << " times to find the range \n";
+
 	SetConsoleTextAttribute(hCon, FOREGROUND_GREEN | FOREGROUND_BLUE);
+
 	cout << "The maximum number was " << MaxNumber << endl;
+
 	SetConsoleTextAttribute(hCon, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+
 	cout << "The Minimum was " << MinNumber << endl;
+
 	system("pause");
 }
