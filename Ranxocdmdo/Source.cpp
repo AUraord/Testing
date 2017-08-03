2aa900b76d27ebc3bfddac1a0b6323e73571806c
alop125125 <alop125125@gmail.com>
2017-08-03 14:48:02 -0600

    i ned this file

--- Ranxocdmdo/Source.cpp
+++ Ranxocdmdo/Source.cpp
@@ -3,7 +3,45 @@
 
 using namespace std;
 
+class Ange
+{
+private:
+	int age;
+	char* name;
+public:
+	void SetAge(int val)
+	{
+		age = val;
+	}
+	void SetName(char* val)
+	{
+		name = val;
+	}
+	int GetAge()
+	{
+		return age;
+	}
+	char* GetName()
+	{
+		return name;
+	}
+};
+
+void HYF(Ange* ag)
+{
+	Ange* LOL = ag;
+
+	LOL->SetName("bob");
+	cout << LOL->GetName() << endl;
+}
+
 int main()
 {
+	Ange Kat;
 
+	HYF(&Kat);
+
+	cout << Kat.GetName() << endl;
+
+	system("pause");
 }
